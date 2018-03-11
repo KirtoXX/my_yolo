d2ac06b97b69194b5599361836e42e17170bc86d
kirto <ljw1995622@126.com>
2018-03-11 13:39:35 +0800

    updata training file

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
+from __future__ import print_function
 from keras.models import Model
 from keras import layers
 import losses
 import yolo
 import dataset
 import tensorflow as tf
+from keras import callbacks
+import numpy as np
 
 classs = 3
 anchor_box = 5
@@ -13,7 +16,7 @@ output_shape = int(input_shape/down_sample)
 
 flags = tf.app.flags
 flags.DEFINE_integer('batch_size',1,'batch_size')
-flags.DEFINE_string('weight','false','weight')
+flags.DEFINE_string('weight','false','weights')
 flags.DEFINE_integer('epoch',50,'epoch')
 flags.DEFINE_float('lr',1e-5,'lr')
 FLAGS = flags.FLAGS
@@ -24,6 +27,14 @@ def main(_):
     weight = FLAGS.weight
     lr = FLAGS.lr
     #input = Input(shape=(416,416,3))
+    m = len(dataset.get_id())
+    steps_per_epoch = int(np.ceil(m/ (float(batch_size))))
+
+    ckpt = callbacks.ModelCheckpoint(filepath='weights/last_weight.h5',
+                                     period=1,
+                                     save_weights_only=True)
+
+
     img,lable = dataset.get_dataset(epoch=epoch,
                                     batch_size=batch_size)
 
@@ -35,7 +46,13 @@ def main(_):
                   loss=losses.custom_loss,
                   target_tensors=[lable])
 
-    model.fit(epochs=epoch,steps_per_epoch=100,verbose=1)
+    if weight != 'false':
+        model.load_weights(weight)
+
+    model.fit(epochs=epoch,
+              steps_per_epoch=steps_per_epoch,
+              verbose=1,
+              callbacks=[ckpt])
 
 if __name__ == '__main__':
     tf.app.run()
